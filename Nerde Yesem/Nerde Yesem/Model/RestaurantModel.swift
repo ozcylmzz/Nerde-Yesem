d7d58c34479179cc7df7703bf923eb2b24bb860a
Özcan Yılmaz <yilmaz.ozcn@gmail.com>
2020-12-10 14:01:29 +0300

    Connection api to app
    Getting restaurants whose locations are close to user location has been done

---
+++ Nerde Yesem/Nerde Yesem/Model/RestaurantModel.swift
@@ -0,0 +1,14 @@
+//
+//  RestaurantModel.swift
+//  Nerde Yesem
+//
+//  Created by Özcan Yılmaz on 10.12.2020.
+//
+
+struct Restaurant {
+    let name: String
+    let address: String
+    let averageCostForTwo: String
+    let aggregateRating: String
+    let img: String
+}
--- Nerde Yesem/Nerde Yesem/ViewControllers/LocationViewController.swift
+++ Nerde Yesem/Nerde Yesem/ViewControllers/LocationViewController.swift
@@ -10,46 +10,38 @@ import CoreLocation
 
 class LocationViewController: UIViewController, CLLocationManagerDelegate {
     
-//    @IBOutlet weak var allowButton: UIButton!
-//    @IBOutlet weak var denyButton: UIButton!
-    
     var locationManager = CLLocationManager()
+    var restaurantService = RestaurantService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
+
     }
-    
-    
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .denied || status == .restricted || status == .notDetermined {
             locationManager.requestWhenInUseAuthorization()
             locationManager.requestLocation()
         } else {
-            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
             let nextViewController = storyBoard.instantiateViewController(withIdentifier: "RestaurantViewController") as! RestaurantViewController
             self.present(nextViewController, animated:true, completion:nil)
         }
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error)
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
-            let lat = location.coordinate.latitude
-            let lon = location.coordinate.longitude
+            let lat: String = location.coordinate.latitude.description
+            let lon: String = location.coordinate.longitude.description
             print(lat)
             print(lon)
+//            restaurantService.fetchData(_latitute: lat, _longitude: lon)
+            restaurantService.fetchData(_latitute: "41.01", _longitude: "28.97")
         }
     }
-//    @IBAction func allowAction(_ sender: UIButton){
-//
-//    }
-//
-//    @IBAction func denyAction(_ sender: UIButton){
-//// uygulamayı kapat
-//    }
 }
